8f5284bcfe7be3765e74b4bfd1e83c837d38487e
Gabriel Amaral <gabrielamaralantuness@gmail.com>
2026-05-06 15:56:02 -0300

    projeção dos dados do autor com DTO

--- mongoDB/src/main/java/com/gabrielamaral/mongoDB/domain/Post.java
+++ mongoDB/src/main/java/com/gabrielamaral/mongoDB/domain/Post.java
@@ -1,5 +1,6 @@
 package com.gabrielamaral.mongoDB.domain;
 
+import com.gabrielamaral.mongoDB.dto.AuthorDTO;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -18,7 +19,7 @@ public class Post implements Serializable {
     private Date date;
     private String title;
     private String body;
-    private User author;
+    private AuthorDTO author;
 
 
 
@@ -26,7 +27,7 @@ public class Post implements Serializable {
 
     }
 
-    public Post(String id, Date date, String title, String body, User author) {
+    public Post(String id, Date date, String title, String body, AuthorDTO author) {
         this.date = date;
         this.id = id;
         this.title = title;
@@ -70,11 +71,11 @@ public class Post implements Serializable {
         this.id = id;
     }
 
-    public User getAuthor() {
+    public AuthorDTO getAuthor() {
         return author;
     }
 
-    public void setAuthor(User author) {
+    public void setAuthor(AuthorDTO author) {
         this.author = author;
     }
 
---
+++ mongoDB/src/main/java/com/gabrielamaral/mongoDB/dto/AuthorDTO.java
@@ -0,0 +1,41 @@
+package com.gabrielamaral.mongoDB.dto;
+
+import com.gabrielamaral.mongoDB.domain.User;
+
+import java.io.Serializable;
+
+public class AuthorDTO implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+
+    private String id;
+    private String name;
+
+    public AuthorDTO() {
+
+    }
+
+    public AuthorDTO(User obj) {
+
+        id = obj.getId();
+        name = obj.getName();
+
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
