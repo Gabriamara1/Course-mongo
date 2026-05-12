cac9e068a8183142fbe833e54610ef7e7a63b5f5
Gabriel Amaral <gabrielamaralantuness@gmail.com>
2026-05-12 15:07:11 -0300

    Consulta simples com @Query

--- mongoDB/src/main/java/com/gabrielamaral/mongoDB/repository/PostRepository.java
+++ mongoDB/src/main/java/com/gabrielamaral/mongoDB/repository/PostRepository.java
@@ -3,12 +3,15 @@ package com.gabrielamaral.mongoDB.repository;
 import com.gabrielamaral.mongoDB.domain.Post;
 import com.gabrielamaral.mongoDB.domain.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
+     @Query("{ 'title': { $regex: ?0, $options: 'i'} }")
+     List<Post> findByTitle(String text);
 
      List<Post> findByTitleContainingIgnoreCase(String text);
 }
